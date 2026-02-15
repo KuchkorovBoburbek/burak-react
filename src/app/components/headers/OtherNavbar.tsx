a2830f90da67505996f856b8fa83128d0bd689f0
Boburbek Kuchqorov <bekkr0321@gmail.com>
2026-02-15 21:20:46 +0900

    feat: publish otherNavbar header component

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,9 +1,77 @@
 import React from 'react'
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 function OtherNavbar() {
+  const authUser = true;
   return (
-    <div>OtherNavbar</div>
-  )
+    <div className="other-navbar">
+      <Container
+        sx={{ mt: "55px", height: "642px" }}
+        className="navbar-container"
+      >
+        <Stack className="menu">
+          <Box>
+            <NavLink to={"/"}>
+              <img
+                className="brand-logo"
+                src="/icons/burak.svg"
+                alt="burakLogo"
+              />
+            </NavLink>
+          </Box>
+          <Stack className="nav-links">
+            <Box className={"hover-line"}>
+              <NavLink to="/">
+                Home
+              </NavLink>
+            </Box>
+            <Box className={"hover-line"}>
+              <NavLink activeClassName="underline" to="/products">
+                Products
+              </NavLink>
+            </Box>
+
+            {authUser ? (
+              <Box className={"hover-line"}>
+                <NavLink activeClassName="underline" to="/orders">
+                  Products
+                </NavLink>{" "}
+              </Box>
+            ) : null}
+
+            {authUser ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/member-page">My page</NavLink>
+              </Box>
+            ) : null}
+            <Box className={"hover-line"}>
+              <NavLink activeClassName="underline" to="/help">
+                Help
+              </NavLink>
+            </Box>
+
+            {/** BASKET */}
+            {!authUser ? (
+              <Button
+                className="login-button"
+                variant="contained"
+                style={{ background: "#3776CC" }}
+              >
+                Login
+              </Button>
+            ) : (
+              <img
+                className="user"
+                src="/icons/default-user.svg"
+                aria-haspopup={"true"}
+              />
+            )}
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
 
 export default OtherNavbar
