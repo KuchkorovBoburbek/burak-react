85c139a52520c0798d282e822218dadd06ac99c8
Boburbek Kuchqorov <bekkr0321@gmail.com>
2026-02-18 17:39:33 +0900

    feat: develop active users sectional component

--- src/app/screens/homePage/ActiveUser.tsx
+++ src/app/screens/homePage/ActiveUser.tsx
@@ -1,12 +1,51 @@
-import { Container } from '@mui/material';
-import React from 'react'
+import { AspectRatio, CardOverflow, CssVarsProvider } from "@mui/joy";
+import Card from "@mui/joy/Card";
+import { Box, Container, Stack, Typography } from "@mui/material";
+
+const activeUsers = [
+  { memberNick: "Martin", memberImage: "/img/martin.webp" },
+  { memberNick: "Justin", memberImage: "/img/justin.webp" },
+  { memberNick: "Rose", memberImage: "/img/rose.webp" },
+  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
+];
 
 function ActiveUser() {
   return (
-    <div>
-      <Container>ActiveUser</Container>
+    <div className="active-users-frame">
+      <Container>
+        <Stack className="main">
+          <Box className="category-title">Active User</Box>
+
+          <Stack className="cards-frame">
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className="card">
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img src={ele.memberImage} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+                      <CardOverflow variant="soft" className="card-detail">
+                        <Box className="member-nickname ">
+                          <Typography className="title">
+                            {ele.memberNick}
+                          </Typography>
+                        </Box>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">No Active Users</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
 
-export default ActiveUser
+export default ActiveUser;
